31593d52212295d913a89f416f5e758e85cc577e
kartikey kamal <kamalkartikey70@gmail.com>
2025-06-15 13:32:29 +0530

    test first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import PracticeCode from "./practiceCode"
+ 
+
+export default function App() {
+  return (
+    <>
+      <h1 className="test">New Tag</h1>
+      <PracticeCode />
+    </>
+  )
+}
+
---
+++ src/practiceCode.jsx
@@ -0,0 +1,7 @@
+function Practice(){
+    return (
+        <h1>New Check</h1>
+    )
+}
+
+export default Practice;
